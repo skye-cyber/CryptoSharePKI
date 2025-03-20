0295bf82c262c0239bb7cc6037b178f90eaeef90
Skye <swskye17@gmail.com>
2025-03-20 12:50:03 +0300

    Add config for setting shared folder
    Signed-off-by: Skye <swskye17@gmail.com>

--- ShareKit/EFS.py
+++ ShareKit/EFS.py
@@ -2,15 +2,41 @@ import os
 import zipfile
 import time
 import magic
+import configparser
 from flask import (Flask, after_this_request, render_template, request, jsonify,
                    send_from_directory, abort)
 from pathlib import Path
 from .net_scanner import main as scanner
 
 app = Flask(__name__)
-UPLOAD_FOLDER = "~/Music"  # "/shared_docs"
-UPLOAD_FOLDER = os.path.expanduser(UPLOAD_FOLDER)  # Expand the user directory
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+# --------------Create a ConfigParser object--------------
+config = configparser.ConfigParser()
+
+# --------------Read the .ini file--------------
+conf_path = os.path.join(os.path.relpath(Path(__file__).parent), "sharekit-conf.ini")
+config.read(conf_path)
+
+
+# --------------Helper function to set a default section--------------
+def set_default_section(config, section):
+    if section not in config:
+        raise ValueError(
+            f"Section '{section}' does not exist in the configuration.")
+    return lambda key: config.get(section, key)
+
+
+# --------------Set default section to RootCAserver--------------
+get_value = set_default_section(config, 'ShareKit')
+
+
+SHARED_FOLDER = get_value("shared-Folder")
+if not SHARED_FOLDER:
+    env = os.getenv("ShareKit_Shared_Folder")
+    SHARED_FOLDER = env if env else "~/Documents"
+
+SHARED_FOLDER = os.path.expanduser(SHARED_FOLDER)  # Expand the user directory
+os.makedirs(SHARED_FOLDER, exist_ok=True)
 
 template = 'index.html'
 
@@ -18,7 +44,7 @@ app = Flask(__name__, template_folder="App/templates",
             static_folder="App/static")  # Custom static folder)
 
 devices = []
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# app.config['SHARED_FOLDER'] = SHARED_FOLDER
 
 
 @app.route('/<path:dir>', methods=['GET'])
@@ -48,7 +74,7 @@ def open_svg_dir(dir):
 def upload_file():
     try:
         file = request.files['file']
-        file.save(os.path.join(UPLOAD_FOLDER, file.filename))
+        file.save(os.path.join(SHARED_FOLDER, file.filename))
         return "File uploaded successfully!", 200
     except Exception as e:
         print(str(e))
@@ -58,12 +84,12 @@ def upload_file():
 @app.route('/download/<filename>', methods=['GET'])
 def download_file(filename):
     try:
-        fpath = os.path.join(os.path.join(UPLOAD_FOLDER, filename))
+        fpath = os.path.join(os.path.join(SHARED_FOLDER, filename))
         if os.path.isfile(fpath):
-            return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
+            return send_from_directory(SHARED_FOLDER, filename, as_attachment=True)
         elif os.path.isdir(fpath):
             # Create a ZipFile object in write mode
-            output_zip = os.path.join(UPLOAD_FOLDER, f"{filename}.zip")
+            output_zip = os.path.join(SHARED_FOLDER, f"{filename}.zip")
 
             with zipfile.ZipFile(output_zip, 'w') as zipf:
                 # Walk through the directory and add files to the zip
@@ -77,7 +103,7 @@ def download_file(filename):
             # Function to delete the zip file after it has been sent
             @after_this_request
             def remove_file(response):
-                output_zip_path = os.path.join(UPLOAD_FOLDER, output_zip)
+                output_zip_path = os.path.join(SHARED_FOLDER, output_zip)
                 try:
                     while True:
                         # Ensure the response is complete before deleting the file
@@ -93,7 +119,7 @@ def download_file(filename):
                     print(e)
                 return response
 
-            return send_from_directory(UPLOAD_FOLDER, os.path.basename(output_zip), as_attachment=True)
+            return send_from_directory(SHARED_FOLDER, os.path.basename(output_zip), as_attachment=True)
         else:
             return "File or directory not found.", 404
     except Exception as e:
@@ -107,7 +133,7 @@ def is_directory(path):
     try:
         is_dir = False
         path = path[1:] if path.startswith('/') else path
-        fpath = os.path.join(UPLOAD_FOLDER, path)
+        fpath = os.path.join(SHARED_FOLDER, path)
         is_dir = os.path.isdir(fpath)
         return is_dir
     except Exception as e:
@@ -128,7 +154,7 @@ def get_mime_type(path):
     """
     try:
         mime = magic.Magic(mime=True)
-        mime_type = mime.from_file(os.path.join(UPLOAD_FOLDER, path))
+        mime_type = mime.from_file(os.path.join(SHARED_FOLDER, path))
         return mime_type.split('/')[0] or 'application'
     except Exception as e:
         print(str(e))
@@ -136,7 +162,7 @@ def get_mime_type(path):
 
 
 @app.template_filter('split')
-def split_str(obj, delimiter=UPLOAD_FOLDER):
+def split_str(obj, delimiter=SHARED_FOLDER):
     _new = obj.split(delimiter)[-1]
     _str = _new[1:] if _new.startswith('/') else _new
     return _str
@@ -145,7 +171,7 @@ def split_str(obj, delimiter=UPLOAD_FOLDER):
 @app.route('/view/<path:dir>', methods=['GET'])
 def open_dir(dir):
     try:
-        path = os.path.join(UPLOAD_FOLDER, dir)
+        path = os.path.join(SHARED_FOLDER, dir)
 
         if not os.path.exists(path):  # Check if path exists
             abort(404)  # Return 404 if path doesn't exist
@@ -180,7 +206,7 @@ def net_devices():
 @app.route('/<device>')
 def show_device_shares(device):
     try:
-        files = os.listdir(UPLOAD_FOLDER)
+        files = os.listdir(SHARED_FOLDER)
         return render_template(template, files=files, os=os)
     except Exception as e:
         print(str(e))
@@ -192,7 +218,7 @@ def get_shared_files():
     try:
         files_ls = []
         files_ls = [file for _, _, files in os.walk(
-            UPLOAD_FOLDER) for file in files]
+            SHARED_FOLDER) for file in files]
         return jsonify(files_ls)
     except Exception as e:
         print(str(e))
@@ -203,7 +229,7 @@ def get_shared_files():
 def get_shared_file_paths():
     try:
         files_ls = []
-        for root, dirs, files in os.walk(UPLOAD_FOLDER):
+        for root, dirs, files in os.walk(SHARED_FOLDER):
             for file in files:
                 _files = os.path.join(root, file)
                 files_ls.append(_files)
@@ -216,7 +242,7 @@ def get_shared_file_paths():
 @app.route('/shared/list_dir/<dir>')
 def _list_dir(dir):
     try:
-        for root, dirs, files in os.walk(UPLOAD_FOLDER):
+        for root, dirs, files in os.walk(SHARED_FOLDER):
             for _dir in dirs:
                 if str(_dir) == str(dir):
                     f_dir = os.path.join(root, _dir)
